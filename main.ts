4014d4751c712e8db8839bcc07d87c99af9d78f9
ninjahru <74370809+ninjahru@users.noreply.github.com>
2021-01-14 14:24:45 -0800

    Update main.ts

--- main.ts
+++ main.ts
@@ -45,3 +45,28 @@ player2 = sprites.create(img`
 player2.setPosition(100, 50)
 controller.player2.moveSprite(player2)
 player2.setFlag(SpriteFlag.StayInScreen, true)
+
+snack = sprites.create(img`
+    . . . . . . . 6 . . . . . . . .
+    . . . . . . 8 6 6 . . . 6 8 . .
+    . . . e e e 8 8 6 6 . 6 7 8 . .
+    . . e 2 2 2 2 e 8 6 6 7 6 . . .
+    . e 2 2 4 4 2 7 7 7 7 7 8 6 . .
+    . e 2 4 4 2 6 7 7 7 6 7 6 8 8 .
+    e 2 4 5 2 2 6 7 7 6 2 7 7 6 . .
+    e 2 4 4 2 2 6 7 6 2 2 6 7 7 6 .
+    e 2 4 2 2 2 6 6 2 2 2 e 7 7 6 .
+    e 2 4 2 2 4 2 2 2 4 2 2 e 7 6 .
+    e 2 4 2 2 2 2 2 2 2 2 2 e c 6 .
+    e 2 2 2 2 2 2 2 4 e 2 e e c . .
+    e e 2 e 2 2 4 2 2 e e e c . . .
+    e e e e 2 e 2 2 e e e c . . . .
+    e e e 2 e e c e c c c . . . . .
+    . c c c c c c c . . . . . . . .
+`)
+snack.setPosition(70, 95)
+
+scene.setBackgroundColor(7)
+
+info.setScore(0)
+info.player2.setScore(0)
